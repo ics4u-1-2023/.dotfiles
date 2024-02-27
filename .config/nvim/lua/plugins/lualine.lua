c978516963e0ed259d71e1ee41cac5154114deac
ics4u-1-2023 <159574064+ics4u-1-2023@users.noreply.github.com>
2024-02-27 09:02:40 -0500

    lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+-- luacheck: ignore vim
+return {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "gruvbox_dark"
+        },
+      })
+    end
+  }
+  
