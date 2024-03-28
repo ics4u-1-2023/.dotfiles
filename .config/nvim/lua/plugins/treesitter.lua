e89d2d1c879a0ef8d5e86051b6c03242ec3e1405
Mr Coxall <mr-coxall>
2024-03-28 08:55:07 -0400

    add autopairs

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+-- luacheck: ignore vim
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = "all",
+      highlight = {enable = true,},
+      indent = {enable = true,},
+    })
+  end
+}
+
